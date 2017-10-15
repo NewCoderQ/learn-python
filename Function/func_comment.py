365247eb31b80d51ea738f32947b6bb1b912c695
funny_QZQ <qiaozhiqiang1993@163.com>
2017-10-15 11:09:28 +0800

    add the func_comment.py file

---
+++ Function/func_comment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: Zhiqiang
+# @Date:   2017-10-15 11:06:41
+# @Last Modified by:   Zhiqiang
+# @Last Modified time: 2017-10-15 11:08:15
+
+def add(a, b):
+	'''Add two numbers
+		
+		Parameters:
+			a: the first number
+			b: the second number
+
+		Returns:
+			the sum of a and b		
+	'''
+
+	return a + b
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Learning Python
 
-## Function:是一个关于python中函数的使用需要注意的事项
+## Function:是一个关于python中函数的使用需要注意的事项的文件夹
 
 * func_comment.py:函数注释
